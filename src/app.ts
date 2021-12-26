727491335c8f72ebe0547594df9121839d868b9b
synchronEnginee <te2.9niku.41cd@gmail.com>
2021-12-26 22:16:23 +0900

    use ! and as

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,14 @@
-// ここにコードを書きます
+// 必ず取得できるもの(null)でないとしたいものに!を付ける
+const form = document.querySelector('form')!
+// as HTMLInputElementをつけることでaddressInputのvalueというプロパティが存在することを指定できる
+const addressInput = document.getElementById('address')! as HTMLInputElement
+
+function searchAddressHandler(event: Event) {
+    event.preventDefault()
+    const enteredAddress = addressInput.value
+
+    //Google API call to
+    
+}
+
+form.addEventListener('submit', searchAddressHandler)
