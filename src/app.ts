4d056f030cf04a9f4a2649467368a62630a70059
synchronEnginee <te2.9niku.41cd@gmail.com>
2021-12-30 15:02:08 +0900

    response use type

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,5 @@
+import axios from 'axios';
+import {YOUR_API_KEY} from './api_key';
 // 必ず取得できるもの(null)でないとしたいものに!を付ける
 const form = document.querySelector('form')!
 // as HTMLInputElementをつけることでaddressInputのvalueというプロパティが存在することを指定できる
@@ -8,6 +10,16 @@ function searchAddressHandler(event: Event) {
     const enteredAddress = addressInput.value
 
     //Google API call to
+    //<{results: {geometry: {location: {lat: number, lng: number}}}[]}>をつけることで、resoponseのresultsの型の自動補完が効くようになる
+    axios.get<{results: {geometry: {location: {lat: number, lng: number}}}[]}>(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        enteredAddress,
+    )}&key=${YOUR_API_KEY}`,
+    ).then(response => {
+        const cordinates = response.data.results[0].geometry.location;
+    }).catch(err => {
+        alert(err.message)
+        console.log(err)
+    })
     
 }
 
